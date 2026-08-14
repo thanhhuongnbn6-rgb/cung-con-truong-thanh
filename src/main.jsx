b43a49505133ac821a9fb57e0de9c176ec4af4b2
thanhhuongnbn6-rgb <thanhhuongnbn6@gmail.com>
2026-08-14 21:23:13 +0700

    Add/Update src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ClassProvider } from './context/ClassContext';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ClassProvider>
+          <App />
+        </ClassProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
